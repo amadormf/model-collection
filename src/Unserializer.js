9fd291bc942c04b6e47d6cd1b0623053417d70d5
Amador Mateo Fernandez <amateofernandez@gmail.com>
2016-04-19 20:37:32 +0200

    Change class for unserializer for array of functions

--- src/Unserializer.js
+++
@@ -1,9 +0,0 @@
-export default class Unserializer {
-  constructor(options = {}) {
-    this.options = options;
-  }
-
-  run() {
-    throw new Error('Override this functions in your class');
-  }
-}
